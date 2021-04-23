bb6acb3fcee1cb765a6635b3046f09e7c326fec4
Nurdaulet <nurzzzone@gmail.com>
2021-04-23 16:07:30 +0600

    refactor: add service command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\CreateViewCommand::class,
+        \App\Console\Commands\CreateServiceCommand::class,
     ];
 
     /**
